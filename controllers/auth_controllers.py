7e46c41887caee8418fa0199240bf74870c62fc2
Kenneth Hefford <kennethhefford@outlook.com>
2024-10-06 19:22:27 +1100

    Added login route to the auth controller

--- controllers/auth_controllers.py
+++ controllers/auth_controllers.py
@@ -3,7 +3,8 @@ from models.user import User, user_schema
 from init import bcrypt, db
 from sqlalchemy.exc import IntegrityError
 from psycopg2 import errorcodes
-
+from flask_jwt_extended import create_access_token
+from datetime import timedelta
 auth_bp = Blueprint("auth", __name__, url_prefix="/auth")
 
 @auth_bp.route("/register", methods=["POST"])
@@ -36,7 +37,23 @@ def register_user():
             return {"error": "Email adress is already in use."}, 400
 
 
-
+@auth_bp.route("/login", methods=["POST"])
+def login_user():
+    #get the data from the body from the body of the request
+    body_data = request.get_json()
+    #find the user in DB with that email address
+    stmt = db.select(User).filter_by(user_name=body_data["user_name"])
+    user = db.session.scalar(stmt)
+    #if user exists and pw is correct 
+    if user and bcrypt.check_password_hash(user.password, body_data.get("password")):
+        #create jwt
+        token = create_access_token(identity=str(user.user_name), expires_delta=timedelta(days=1))
+        #respond
+        return {"email": user.email, "is_admin": user.is_admin, "token": token}
+    #else
+    else:
+        #respond back with an error message
+        return {"error": "Invalid email or password"}, 400
 
 
 
